b7f986d711d5b92d56203a07622c9b5ab45ec9fe
Juliya Boiko <boikojuliya2707@gmail.com>
2023-04-24 13:15:03 +0300

    update Readme

--- README.md
+++ README.md
@@ -1,8 +1,3 @@
-# React homework template
+# V-Transport
 
-Этот проект был создан при помощи
-[Create React App](https://github.com/facebook/create-react-app). Для знакомства
-и настройки дополнительных возможностей
-[обратись к документации](https://facebook.github.io/create-react-app/docs/getting-started).
-
-## Создание репозитория по шаблону
+Transportation servise
--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -3,28 +3,16 @@ import { Header } from 'components/Header';
 const Homepage = () => {
 
   return (
-    <div className="homepage">
+    <>
       <Header />
-      <h1>Homepage:</h1>
-    </div>
+      <main className="homepage">
+        <h1 className='homepage__title'>
+          Welcome to <br /> V-Transport
+        </h1>
+        <p>service for passengers and drivers</p>
+      </main>
+    </>
   );
 };
 
 export default Homepage;
-
-
-// import { updateProfile } from "firebase/auth";
-
-
-  // const updateName = async () => {
-  //   await updateProfile(authentication.currentUser, {
-  //     displayName: "Jane Q. User", photoURL: ""
-  //   }).catch((error) => {
-  //     console.log(error);
-  //   });
-  //   dispatch(getCurrentUser());
-  // };
-
-
-      // {/* <button type="button" onClick={updateName}>update</button>
-      // <button type="button" onClick={() => console.log('check', authentication.currentUser)}>Check</button> */}
