50e2503f1b0c4b2e711e265aa7f9721eeb082062
Juliya Boiko <boikojuliya2707@gmail.com>
2023-04-24 13:48:55 +0300

    add redux-persist

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 import { App } from 'components/App';
-import { store } from 'redux/store';
+import { store, persistor } from 'redux/store';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'modern-normalize';
 import '../node_modules/react-bootstrap/dist/react-bootstrap.min';
@@ -13,9 +14,11 @@ import './services/notifyConfig';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename='/transportation'>
-        <App />    
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter basename='/transportation'>
+          <App />    
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,11 @@
-//import storage from 'redux-persist/lib/storage';
+import storage from 'redux-persist/lib/storage';
 import { configureStore } from "@reduxjs/toolkit";
 import { authSlice } from "./auth/authSlice";
 import { tripsSlice } from "./trips/tripsSlice";
 import { usersSlice } from "./users/usersSlice";
 // import { charactersSlice } from "./characters/charactersSlice";
 // import { authSlice } from "./auth/authSlice";
-// import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
 
 // const charactersPersistConfig = {
 //   key: 'characters',
@@ -13,25 +13,25 @@ import { usersSlice } from "./users/usersSlice";
 //   whitelist: ['filter', 'items'],
 // };
 
-// const authPersistConfig = {
-//   key: 'auth',
-//   storage,
-//   whitelist: ['displayName', 'uid', 'accesToken', 'isLogged'],
-// };
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['uid', 'accesToken', 'isLogged'],
+};
 
 export const store = configureStore({
   reducer: {
-    //characters: persistReducer(charactersPersistConfig, charactersSlice.reducer),
-    auth: authSlice.reducer,
+    auth: persistReducer(authPersistConfig, authSlice.reducer),
+    //auth: authSlice.reducer,
     trips: tripsSlice.reducer,
     users: usersSlice.reducer,
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
-  // middleware: getDefaultMiddleware => getDefaultMiddleware({
-  //   serializableCheck: {
-  //       ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-  //   },
-  // }),
+  //middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
+  middleware: getDefaultMiddleware => getDefaultMiddleware({
+    serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
 });
 
-//export const persistor = persistStore(store);
+export const persistor = persistStore(store);
